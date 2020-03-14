556f5b2045402161d3e7acbbd21fc53d1ccfb8b5
Matija Baskarad (MacBook Privat) <matibaski@gmail.com>
2020-03-14 07:55:14 +0100

    Fixed login & register templates, removed 3rd-party-plugins (will be copied by composer-copy-file to webroot)

--- templates/Users/register.php
+++ templates/Users/register.php
@@ -12,13 +12,9 @@
 
 	<title><?= __('Register') . ' | ' . $settings['backend_name'] ?></title>
 
-	<!-- Custom fonts for this template-->
-	<link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-	<!-- Custom styles for this template-->
-	<link href="/css/sb-admin-2.min.css" rel="stylesheet">
-
+	<?= $this->Html->css('theme.min.css') ?>
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
 </head>
 
 <body class="bg-gradient-primary">
@@ -108,19 +104,12 @@
 				</div>
 			</div>
 		</div>
-
 	</div>
 
-	<!-- Bootstrap core JavaScript-->
-	<script src="/vendor/jquery/jquery.min.js"></script>
-	<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-	<!-- Core plugin JavaScript-->
-	<script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-	<!-- Custom scripts for all pages-->
-	<script src="/js/sb-admin-2.min.js"></script>
-
+	<script src="/plugins/jquery/jquery.min.js"></script> 
+    <script src="/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="/plugins/jquery-easing/jquery.easing.min.js"></script>
+    <script src="/js/main.js"></script>
+    <script src="/js/theme.js"></script>
 </body>
-
 </html>
