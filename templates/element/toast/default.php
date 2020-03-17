c0445bf73e1643509d3c888c00dfd35c5328f697
Matija Baskarad (MacBook Privat) <matibaski@gmail.com>
2020-03-17 06:07:33 +0100

    Created "Toast" function (analog to flash)

---
+++ templates/element/toast/default.php
@@ -0,0 +1,11 @@
+<div class="toast" role="alert" aria-live="polite" aria-atomic="true" data-delay="10000">
+    <div class="toast-header">
+        <i class="far fa-bell fa-fw mr-2"></i> <strong>Notification</strong>
+        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="toast-body">
+        <p><?= $message ?></p>
+    </div>
+</div>
