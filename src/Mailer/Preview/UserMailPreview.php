b00bac8fc8a22541b5c46c21df8dda847615a22f
Matija Baskarad (MacBook Privat) <matibaski@gmail.com>
2020-03-19 13:36:24 +0100

    Fixed BaseHelper and Mailpreview.

--- src/Mailer/Preview/UserMailPreview.php
+++ src/Mailer/Preview/UserMailPreview.php
@@ -3,21 +3,22 @@
 namespace App\Mailer\Preview;
 
 use DebugKit\Mailer\MailPreview;
+use Cake\ORM\TableRegistry;
 
 class UserMailPreview extends MailPreview
 {
 
     public function welcome()
     {
-        $this->loadModel('Users');
-        $user = $this->Users->find()->first();
+        $user = TableRegistry::getTableLocator()->get('Users');
+        $user = $user->find()->first();
         return $this->getMailer('User')->welcome($user);
     }
 
     public function forgot()
     {
-        $this->loadModel('Users');
-        $user = $this->Users->find()->first();
+        $user = TableRegistry::getTableLocator()->get('Users');
+        $user = $user->find()->first();
         return $this->getMailer('User')->forgot($user, 'testpassword123');
     }
 }
